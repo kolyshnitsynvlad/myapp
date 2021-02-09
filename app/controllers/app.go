ac80358d084517937f4c10a5b8be7375c30d00a6
Kolysh <KolyshnitsynVlad@yandex.ru>
2021-02-09 19:15:25 +0300

    поиск

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -8,20 +8,43 @@ type App struct {
 	*revel.Controller
 }
 
-func (c App) Index() revel.Result {
+func (c App) Index(Author string, BookName string, Publisher string) revel.Result {
+	c.Log.Debug(Author)
+	c.Log.Debug(BookName)
+	c.Log.Debug(Publisher)
 	type Book struct {
 		Author    string
 		BookName  string
 		Publisher string
 	}
 	//data->>'id', data->>'type', data->>'title' (book_name, author_fio, publisher_name)
-	r, err := DB.Query("select book_name, author_fio, publisher_name " +
+	strselect := "select book_name, author_fio, publisher_name " +
 		"from book " +
 		"join book_author using (book_id) " +
 		"left join author using (author_id) " +
 		"join book_publisher using (book_id) " +
-		"left join publisher using (publisher_id)",
-	)
+		"left join publisher using (publisher_id) "
+	var search string
+	if len(Author) > 0 {
+		search = "where author_fio ilike '%" + Author + "%' "
+	}
+	if len(BookName) > 0 {
+		if len(search) > 0 {
+			search += "and book_name ilike '%" + BookName + "%' "
+		} else {
+			search = "where book_name ilike '%" + BookName + "%' "
+		}
+	}
+	if len(Publisher) > 0 {
+		if len(search) > 0 {
+			search += "and publisher_name ilike '%" + Publisher + "%' "
+		} else {
+			search = "where publisher_name ilike '%" + Publisher + "%' "
+		}
+	}
+	strselect += search
+	c.Log.Debug(strselect)
+	r, err := DB.Query(strselect)
 	if err != nil {
 		return c.RenderError(err)
 	}
@@ -38,6 +61,6 @@ func (c App) Index() revel.Result {
 		books = append(books, b)
 	}
 
-	return c.Render(books)
+	return c.Render(books, Author, BookName, Publisher)
 
 }
